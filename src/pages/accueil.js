54e1949ab00d05cb0b9fff41c83877d5d7861236
Johan Nilsson <johan.nilsson.mail@gmail.com>
2023-10-12 17:29:38 +0200

    feature: add banner + redefine some css properties

--- src/pages/accueil.js
+++ src/pages/accueil.js
@@ -1,8 +1,11 @@
+import Banner from '../components/banner';
 import '../styles/accueil.css';
+import img from '../img/banner1.jpg';
 
 function Accueil() {
   return (
     <div className='container-app'>
+      <Banner src={img} />
       <p>Hello React 😁👌</p>
     </div>
   );
