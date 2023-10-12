ec9f255004ab597fed1a2fde735c089eb222a0e8
Johan Nilsson <johan.nilsson.mail@gmail.com>
2023-10-12 10:35:56 +0200

    (feature):add header and footer

---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+import '../styles/header.css';
+function Header() {
+  return (
+    <div className='header'>
+      <img src={require('../img/LOGO.png')} alt='Logo Kasa'></img>
+    </div>
+  );
+}
+
+export default Header;
