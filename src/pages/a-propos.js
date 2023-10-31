bf448d728be5e56263d8ff2d9cad774c9ce20f99
Johan Nilsson <johan.nilsson.mail@gmail.com>
2023-10-31 15:40:24 +0100

    fix: suppression compteur carousel version mobile et refacto page a-propos

--- src/pages/a-propos.js
+++ src/pages/a-propos.js
@@ -3,27 +3,32 @@ import '../styles/a-propos.css';
 import img from '../img/banner2.jpg';
 import Dropdown from '../components/dropdown';
 
+const dropdownData = [
+  {
+    title: 'Fiabilité',
+    txt: 'Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.',
+  },
+  {
+    title: 'Respect',
+    txt: 'La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.',
+  },
+  {
+    title: 'Service',
+    txt: 'Climatisation Wi-Fi Cuisine Espace de travail Fer à repasser Sèche-cheveux Cintres',
+  },
+  {
+    title: 'Sécurité',
+    txt: "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.",
+  },
+];
+
 function APropos() {
   return (
     <div className='container-app'>
       <Banner src={img} />
-      <Dropdown
-        txt='Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont
- régulièrement vérifiées par nos équipes.'
-        title='Fiabilité'></Dropdown>
-      <Dropdown
-        txt='La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de
-perturbation du voisinage entraînera une exclusion de notre plateforme.'
-        title='Respect'></Dropdown>
-      <Dropdown
-        txt='Climatisation Wi-Fi Cuisine Espace de travail Fer à repasser Sèche-cheveux Cintres'
-        title='Service'></Dropdown>
-      <Dropdown
-        txt="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement
-correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au
-locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons
-également des ateliers sur la sécurité domestique pour nos hôtes."
-        title='Sécurité'></Dropdown>
+      {dropdownData.map((item, index) => (
+        <Dropdown key={index} txt={item.txt} title={item.title} />
+      ))}
     </div>
   );
 }
